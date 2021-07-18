00ea00542f9ec00ce4d8a54f03098624bc41525b
Jaime <ja>
2021-07-17 20:11:21 -0500

    Principio de mutabilidad

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('hola mundo');
+
+let variable ='soy una variable';
+console.log(variable)
+
+variable = "Ahora estoy modificada"; // A esto se le llama mutabilidad
+console.log(variable);
