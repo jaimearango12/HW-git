e5d23327161d40ee72045105dab1954c86587147
Jaime <ja>
2021-07-27 16:18:09 -0500

    falla total
    Proyecto abandonado

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-    res.send('hola mundo')
-}
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser= require('body-parser')
+const cors = require ('cors')
+
+const meals = require('./routes/meals')
+const app = express()
+app.use(bodyParser.json())   // app.use añade plugins
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+
+    app.use('/api/meals', meals)
+
+module.exports = app
---
+++ serverless/api/routes/meals.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+
+const router= app.router()
+
+router.get('/', (req, res)=>{
+    
+})
+
+mongoose.connect
